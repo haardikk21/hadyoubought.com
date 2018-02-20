ebb0e585c69976de0d96a7e6b1b348d1a110e86e
Haardik <hi@haardikk.me>
2018-02-20 18:55:45 -0400

    Solved datepicker bug which chose wrong dates when changing months or years. Fixed website showing 00 for future dates.

--- data/getAmount.php
+++ data/getAmount.php
@@ -5,20 +5,25 @@
 
     $todayTimestamp = strtotime("now");
     $oldTimestamp = strtotime($date);
+    if ($oldTimestamp <= $todayTimestamp)
+    {
+        $todayURL = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=" . strtoupper($coin) . "&tsyms=USD&ts=" . $todayTimestamp;
+        $oldURL = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=" . strtoupper($coin) . "&tsyms=USD&ts=" . $oldTimestamp;
 
-    $todayURL = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=" . strtoupper($coin) . "&tsyms=USD&ts=" . $todayTimestamp;
-    $oldURL = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=" . strtoupper($coin) . "&tsyms=USD&ts=" . $oldTimestamp;
+        $todayJson = file_get_contents($todayURL);
+        $oldJson = file_get_contents($oldURL);
+        
+        $todayPrice = json_decode($todayJson, true)[strtoupper($coin)]["USD"];
+        $oldPrice = json_decode($oldJson, true)[strtoupper($coin)]["USD"];
 
-    $todayJson = file_get_contents($todayURL);
-    $oldJson = file_get_contents($oldURL);
-    
-    $todayPrice = json_decode($todayJson, true)[strtoupper($coin)]["USD"];
-    $oldPrice = json_decode($oldJson, true)[strtoupper($coin)]["USD"];
+        $usdToday = 0;
 
-    $usdToday = 0;
+        $bitcoinValue = 100 / $oldPrice;
+        $usdToday = $todayPrice * $bitcoinValue;
 
-    $bitcoinValue = 100 / $oldPrice;
-    $usdToday = $todayPrice * $bitcoinValue;
-
-    echo $usdToday;
+        echo $usdToday; 
+    }
+    else {
+        echo "err";
+    }
 ?>
