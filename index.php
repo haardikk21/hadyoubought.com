e43b4f8f2bfc4e23bcfac8ce75e45547a9628b65
Haardik <hi@haardikk.me>
2018-02-20 05:12:42 -0400

    Added Fork on Github ribbon

--- index.php
+++ index.php
@@ -21,6 +21,8 @@
       <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
    </head>
    <body>
+
+   <a href="https://github.com/haardikk21/hadyoubought.com/"><img style="z-index: 5;position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
       <div class="header">
         <p>Warning: This website can add to Fear of Missing Out (FOMO) and lead to impulsive actions. Please invest responsibly.</p>
       </div>
